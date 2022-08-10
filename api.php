43f1e03f612292e8a5b972a2dd3a8644e85f6e79
estevaoacioli <estevaoaciolicce@gmail.com>
2022-08-10 14:09:33 -0300

    integrado com api via cep

---
+++ api.php
@@ -0,0 +1,44 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit();
+}
+
+class ConnectViaCep{   
+    
+    public function __construct() {
+        
+        return false;
+		
+	}
+
+    public function get_cep_infos( $tsp_cep_value, $tsp_cep_active ){		
+		
+
+        $cep = str_replace('-', '', $tsp_cep_value);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://viacep.com.br/ws/$cep/json/",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        
+
+        return json_decode($response, true);
+    
+
+    }
+
+
+}
+
--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function tsp_register_mysettings() {
 }
 add_action('admin_init', 'tsp_register_mysettings');
 
-// Function to load the styles
+// Function to load the styles and scripts in back
 function tsp_scripts_admin() {
     global $pagenow;
     $currentScreen = get_current_screen();
@@ -34,3 +34,46 @@ function tsp_scripts_admin() {
     }    
 }
 add_action('admin_enqueue_scripts', 'tsp_scripts_admin');
+
+// Function to load the styles and scripts in back
+function tsp_scripts_front() {
+    
+    if( is_home() || is_front_page() ){
+        
+		// register CSS
+		wp_register_style('tsp_styles', plugin_dir_url(__FILE__) . 'assets/css/tsp-front.css', false, false); 
+		
+		// enqueues CSS
+		wp_enqueue_style('tsp_styles');	
+		
+    }    
+}
+add_action('wp_enqueue_scripts', 'tsp_scripts_front');
+
+
+// Insert HTML in footer
+function tsp_footer_orange_stripe(){	
+	$tsp_cep_value = get_option('tsp_cep_value', false); //var_dump($tsp_cep_value);
+    $tsp_cep_active = get_option('tsp_cep_active', false); //var_dump($tsp_cep_active);
+
+	if( !empty($tsp_cep_value) && $tsp_cep_active == 'yes' ){
+	ob_start();
+	?>
+    <div id="orange_stripe">
+        <div class="content-address">
+            <?php $infos = ConnectViaCep::get_cep_infos( $tsp_cep_value, $tsp_cep_active );  //var_dump($infos);?>
+            <p><?php echo $infos['logradouro']; ?>, <?php echo $infos['localidade']; ?> - <?php echo $infos['uf']; ?></p>
+            <p><?php echo $infos['cep']; ?></p>
+        </div>
+    </div>
+	<?php
+	    $ret = ob_get_contents();
+		
+		/* Clean buffer */
+		ob_end_clean();
+		
+	    echo $ret;
+    }
+}
+add_action( 'wp_footer', 'tsp_footer_orange_stripe');
+
